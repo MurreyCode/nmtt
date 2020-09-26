98cc2a756d0969529413eb298097d60d48eb037a
Pablo Reynel <pablo.reynel@gmail.com>
2020-09-26 14:09:53 +0200

    parse track name and save_meta

--- src/run.py
+++ src/run.py
@@ -2,7 +2,6 @@ from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
 import mutagen.id3
 from mutagen.id3 import ID3, TIT2, TIT3, TALB, TPE1, TRCK, TYER
-
 import glob
 
 
@@ -16,22 +15,33 @@ def get_remixer(name):
     return track, arranger
 
 
+def save_meta(path, meta):
+    try:
+        file = MP3(path, ID3=EasyID3)
+        for key, value in meta.items():
+            file[key] = value
+        print(file)
+        file.save()
+    except Exception as exception:
+        print(exception.__traceback__)
+
+
 if __name__ == '__main__':
 
     mp3_files = glob.glob('/Users/pablo/Music/Pending/*.mp3')
     track_path = mp3_files[0]
     artist_name, track_name = get_artist_track(track_path)
-    clean_track, remixer = get_remixer(track_name) if 'Remix' in track_name else None
 
     print('Artist:', artist_name)
     print('Track:', track_name)
-    meta = {
+
+    clean_track, remixer = get_remixer(track_name) if 'Remix' in track_name else track_name, None
+
+    metadata = {
         'title': [track_name],
         'artist': [artist_name],
         'arranger': [remixer]
     }
 
-    mp3file = MP3(track_path, ID3=EasyID3)
-    # what is contained in the existing ID3 tags?
-    print(mp3file)
+    save_meta(track_path, metadata)
 
